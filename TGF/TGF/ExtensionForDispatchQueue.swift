2cbdc645270146f773a0e6dc3a9d8c7fb7778688
ANDRZEJ SEMENIUK <andrzejsemeniuk@gmail.com>
2017-01-11 07:16:34 -0800

    dev

---
+++ TGF/TGF/ExtensionForDispatchQueue.swift
@@ -0,0 +1,9 @@
+//
+//  ExtensionForDispatchQueue.swift
+//  TGF
+//
+//  Created by andrzej semeniuk on 12/29/16.
+//  Copyright © 2016 Tiny Game Factory LLC. All rights reserved.
+//
+
+import Foundation
---
+++ TGF/TGF/ExtensionForFoundationNSRange.swift
@@ -0,0 +1,9 @@
+//
+//  ExtensionForFoundationNSRange.swift
+//  TGF
+//
+//  Created by andrzej semeniuk on 1/8/17.
+//  Copyright © 2017 Tiny Game Factory LLC. All rights reserved.
+//
+
+import Foundation
